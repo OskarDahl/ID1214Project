28a68dc84f0948d9878f203454b94a50c6c6deaf
thesan <theo.sandstorm@gmail.com>
2017-11-29 13:32:32 +0100

    Created terrain abstract class

---
+++ Maze/src/maze/Terrain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maze;
+
+/**
+ *
+ * @author Theo
+ */
+public abstract class Terrain {
+    
+    public abstract boolean passable();
+    
+    public abstract void print();
+    
+    public abstract void interract();
+}
---
+++ Maze/src/maze/Wall.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maze;
+
+/**
+ *
+ * @author Theo
+ */
+public class Wall extends Terrain {
+
+    @Override
+    public boolean passable() {
+        return false;
+    }
+
+    @Override
+    public void print() {
+        System.out.print("X");
+    }
+
+    @Override
+    public void interract() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
